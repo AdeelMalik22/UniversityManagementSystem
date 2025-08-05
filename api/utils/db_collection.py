173f1959cba385f7c826e351fd6bac68e88342a6
Subhan Adeel <ms1640070@gmail.com>
2025-08-05 18:12:01 +0500

    remove auth

--- api/utils/db_collection.py
+++ api/utils/db_collection.py
@@ -1,6 +1,5 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
 MONGO_DETAILS = "mongodb://myAdminUser:myStrongPassword@localhost:27017/?authSource=admin"
-
-client = AsyncIOMotorClient(MONGO_DETAILS)
+client = AsyncIOMotorClient(...)(MONGO_DETAILS)
 mongodb = client["college"]
